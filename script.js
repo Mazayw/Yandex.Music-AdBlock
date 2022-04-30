da8be637dd371ea0942bd38215e0b7c82a950304
Mazayw <mazayw@gmail.com>
2022-04-30 18:32:57 +0300

    fix: fix errors

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         AdBlock Yandex.Music
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Yandex.Music AdBlock
 // @author       Mazayw
 // @match        https://music.yandex.ru/*
@@ -15,9 +15,11 @@
     setInterval(() => {
         const modalWindow = document.getElementsByClassName('crackdown-popup popup_compact local-theme-white local-icon-theme-white popup deco-pane-popup popup_modal');
         document.getElementsByClassName('bar-below bar-below_plus')[0].classList.add('popup_hidden');
-        if (modalWindow.length && !Object.values(modalWindow[0].classList).includes('popup_hidden')) {
-            document.getElementsByClassName('d-button deco-button deco-button-flat d-button_type_flat d-button_w-icon d-button_w-icon-centered crackdown-popup__close')[0].click();
-            document.getElementsByClassName('player-controls__btn deco-player-controls__button player-controls__btn_play')[0].click();
+        if (modalWindow.length) {
+            if (!Object.values(modalWindow[0].classList).includes('popup_hidden')) {
+                document.getElementsByClassName('d-button deco-button deco-button-flat d-button_type_flat d-button_w-icon d-button_w-icon-centered crackdown-popup__close')[0].click();
+                document.getElementsByClassName('player-controls__btn deco-player-controls__button player-controls__btn_play')[0].click();
+            }
         }
     }, 1000);
 })();
